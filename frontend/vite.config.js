fae36c5d98034358c883fa04f954c1915b35e3de
Manajot17 <mananjotsingh17@gmail.com>
2024-10-02 00:14:30 +0530

    final commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:"https://eventmanagement-production.up.railway.app/",
+        target:"https://eventmanagement-production.up.railway.app",
         changeOrigin: true,
       }
     }
